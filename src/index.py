21c250b204aa58beb5163ea765fbc882db3740d4
Eero Lumijärvi <eero.lumijarvi@helsinki.fi>
2024-11-30 11:18:43 +0200

    Added the functions to delete references from the database. Made a helper function that fills the database with random references.

--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
 from app import app
+from config import test_env
 
 if __name__ == "__main__":
-    app.run(port=5001, host="0.0.0.0", debug=True)
+    app.run(port=5001, host="0.0.0.0", debug=test_env)
